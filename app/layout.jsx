0f6e4131088497e072798b5f648c114cf1aff395
Parag <paragpurandare50@gmail.com>
2024-02-07 20:26:16 +0530

    changes in navbar trying to add auth desktop view completed

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import '@styles/global.css';
-
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
 
 export const metadata = {
       title: "Promptopia",
@@ -13,6 +14,7 @@ const RootLayout = ({ children }) => {
                               <div className='gradient' />
                         </div>
                         <main className='app'>
+                              <Nav />
                               {children}
                         </main>
                   </body>
