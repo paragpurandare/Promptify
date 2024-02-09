08e633e6f5f1afcc6f4605459a4b1df364255ca1
Parag <paragpurandare50@gmail.com>
2024-02-09 15:25:44 +0530

    Created a successful connection to mongoDB

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-
+import { connectToDB } from '@utils/database';
 
 const handler = NextAuth({
       providers: [
@@ -13,6 +13,16 @@ const handler = NextAuth({
 
       },
       async signIn({profile}) {
+            try{
+                  await connectToDB();
+                  //check if a user already exists
+                  //if not, create a new user
+                  
+                  return true;
+            }catch(error){
+                  console.log(error);
+                  return false;
+            }
       }
 })
 
