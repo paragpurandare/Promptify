6504c102fedc9af7058d599fa8296f3da2b5d21d
paragpurandare <paragpurandare50@gmail.com>
2024-03-13 00:21:57 +0530

    changes succesful mongodb connection and dynamically created database to store users info

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -3,40 +3,55 @@ import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
 import User from "@models/user";
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    }),
-  ],
-  async session({ session }) {
-      const sessionUser = await User.findOne({
-            email: session.user.email
-      })
-      session.user.id = sessionUser._id.toString();
-      return session;
-  },
+// Function to generate a valid username based on the user's profile information
+const generateUsername = (email) => {
+	// Extracting the username from the email (before '@')
+	const username = email.split("@")[0];
+	// Trimming and converting to lowercase
+	return username.trim().toLowerCase();
+};
 
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
-      //check if a user already exists
-      const userExists = await User.findOne({ email: profile.email });
-      //if not, create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.pitcure,
-        });
-      }
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
-  },
+const handler = NextAuth({
+	providers: [
+		GoogleProvider({
+			clientId: process.env.GOOGLE_ID,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+		}),
+	],
+	callbacks: {
+		async session({ session }) {
+			const sessionUser = await User.findOne({
+				email: session.user.email,
+			});
+			session.user.id = sessionUser._id.toString();
+			return session;
+		},
+
+		async signIn({ profile }) {
+			try {
+				await connectToDB();
+				// Check if a user already exists
+				const userExists = await User.findOne({ email: profile.email });
+
+				// If not, create a new user
+				if (!userExists) {
+					// Generate a username based on the email
+					const username = generateUsername(profile.email);
+
+					// Create the user with the generated username
+					await User.create({
+						email: profile.email,
+						username: username,
+						image: profile.picture,
+					});
+				}
+				return true;
+			} catch (error) {
+				console.log(error);
+				return false;
+			}
+		},
+	},
 });
 
 export { handler as GET, handler as POST };
