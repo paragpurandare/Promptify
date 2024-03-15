89f3885ba38cd8bd643b2db83aca521173d80b8b
paragpurandare <paragpurandare50@gmail.com>
2024-03-15 20:04:38 +0530

    Completed all crud applications sucessfully on an individual profile giving them functionallity

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
-import { Router } from "next/router";
 
 const MyProfile = () => {
 	const { data: session } = useSession();
@@ -21,18 +20,32 @@ const MyProfile = () => {
 	const handleEdit = (post) => {
 		router.push(`/update-prompt?id=${post._id}`);
 	};
-	const handleDelete = async (post) => {};
-	return (
-		<div>
-			<Profile
-				name="My"
-				desc="Welcome to your personalized profile page"
-				data={posts}
-				handleEdit={handleEdit}
-				handleDelete={handleDelete}
-			/>
-		</div>
-	);
+	const handleDelete = async (post) => {
+		const hasConfirmed = confirm(
+			"Are you sure you want to delete this prompt?"
+		);
+		if (hasConfirmed) {
+			try {
+				await fetch(`/api/prompt/${post._id.toString()}`, {
+					method: "DELETE",
+				});
+				const filteredPosts = posts.filter((p) => p._id !== post._id);
+				setPosts(filteredPosts);
+			} catch (error) {
+				console.log(error);
+			}
+		}
+		return (
+			<div>
+				<Profile
+					name="My"
+					desc="Welcome to your personalized profile page"
+					data={posts}
+					handleEdit={handleEdit}
+					handleDelete={handleDelete}
+				/>
+			</div>
+		);
+	};
 };
-
 export default MyProfile;
