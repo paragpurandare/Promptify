56ad187a25fda0ebf7f5764cec0e496827b71c37
paragpurandare <paragpurandare50@gmail.com>
2024-03-15 18:20:01 +0530

    Initialized the my profile page stucture and functions to be performed created the route for dynamic route uers/id/posts

---
+++ app/profile/page.jsx
@@ -0,0 +1,38 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Profile from "@components/Profile";
+
+const MyProfile = () => {
+	const { data: session } = useSession();
+	const [posts, setPosts] = useState([]);
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const response = await fetch(`api/users/${session?.user.id}/posts`);
+			const data = await response.json();
+			setPosts(data);
+		};
+		if (session?.user.id) fetchPosts();
+	}, []);
+
+	const handleEdit = () => {
+		console.log("Edit button clicked");
+	};
+	const handleDelete = async () => {
+		console.log("Delete button clicked");
+	};
+	return (
+		<div>
+			<Profile
+				name="My"
+				desc="Welcome to your personalized profile page"
+				data={posts}
+				handleEdit={handleEdit}
+				handleDelete={handleDelete}
+			/>
+		</div>
+	);
+};
+
+export default MyProfile;
